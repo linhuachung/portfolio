ab07f18f6f21f71d00f3a647a805353ed0c51cbc
ChungLH <chunglh1304@gmail.com>
2025-03-16 11:33:58 +0700

    Modified input form

--- components/InputField.jsx
+++ components/InputField.jsx
@@ -1,11 +1,31 @@
+import {useState} from "react";
 import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
 
 export function InputField({name, register, errors, placeholder, onBlur}) {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
-        <div>
-            <Input className={`w-full ${errors[name] && "border-red-500"}`} {...register(name)}
-                   placeholder={placeholder}
-                   onBlur={onBlur}/>
+        <div className="relative w-full">
+            <Label
+                htmlFor={name}
+                className={`absolute left-3 transition-all text-gray-500 ${
+                    isFocused || register(name).value ? " z-10 bg-[#27272c] text-white/60 -top-2 text-xs px-2 before:content-[''] before:absolute before:-z-10 before:left-0 before:right-0 before:top-0 before:bottom-0 before:bg-[#27272c] before:rounded-sm" : "top-3 text-sm"
+                } ${errors[name] ? "text-red-500" : ""}`}
+            >
+                {placeholder}
+            </Label>
+            <Input
+                id={name}
+                className={`w-full pt-2 pb-2 bg-transparent ${errors[name] && "border-red-500"} `}
+                {...register(name)}
+                placeholder=""
+                onFocus={() => setIsFocused(true)}
+                onBlur={(e) => {
+                    setIsFocused(e.target.value !== "");
+                    onBlur && onBlur(e);
+                }}
+            />
             {errors[name] && (
                 <p className="text-red-500 text-sm mt-2 ml-1">{errors[name]?.message}</p>
             )}
